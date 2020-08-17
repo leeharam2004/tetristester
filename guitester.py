95bf08480b2ce082f6f06f79b9db9854c730b315
valueOF <leeharam2004@gmail.com>
2020-08-17 11:35:10 +0900

    asdf

--- guitester.py
+++ guitester.py
@@ -1,1 +1,25 @@
+import random
 
+from Settings import Settings
+#from environment.AutoPlayEnvironment import AutoPlayEnvironment
+from environment.ManualPlayEnvironment import ManualPlayer
+from graphics.DummyGraphicModule import DummyGraphicModule
+from graphics.GraphicModule import GraphicModule
+
+settings = Settings()
+
+def random_tetromino():
+    return random.randrange(0, settings.ACTIONS)
+TETROMINO_AGENT = random_tetromino
+
+graphic_interface = GraphicModule(settings)
+
+env_model = ManualPlayer(settings, graphic_interface)
+
+turns = 0
+while True:
+    turns += 1
+    _, _, end = env_model.action(TETROMINO_AGENT())
+    if end:
+        print("Turns: " + str(turns))
+        turns = 0
